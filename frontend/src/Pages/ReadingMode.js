d449fdf47b38307fdceaf8f0fef3f9971b70ea7f
rorycoflynn <143354491+rorycoflynn@users.noreply.github.com>
2025-04-18 10:11:58 -0600

    Merge branch 'origin/main' into rory

--- frontend/src/Pages/ReadingMode.js
+++ frontend/src/Pages/ReadingMode.js
@@ -9,7 +9,7 @@ import ReadingSidebar from '../components/readingmode-components/ReadingSidebar'
 import ReadingContainer from '../components/readingmode-components/ReadingContainer';
 import ReadingModeAnnotator from '../components/readingmode-components/ReadingModeAnnotator';
 import './ReadingMode.css';
->>>>>>> origin/main
+
 
 function ReadingModePage() {
     const [currentPage, setCurrentPage] = useState(1);
